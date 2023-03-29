c8d2010e7745861d669ca65d4a0296de3b2f0b7e
deep-osbert <126625111+deep-osbert@users.noreply.github.com>
2023-03-29 22:18:39 +0200

    Update label_cro.py

--- label_cro.py
+++ label_cro.py
@@ -3,51 +3,19 @@ import time
 import json
 import uuid
 import streamlit as st
-
-# Additional imports for the new features
 from pathlib import Path
 import pandas as pd
 import altair as alt
 
-# Function to highlight text
 def highlight_text(text, highlighted_words):
     for word in highlighted_words:
         text = text.replace(word, f"<span style='color:red;'>{word}</span>")
     return text
 
-# Your original script, wrapped in a function
 def main_app():
-    # ... (All the original code) ...
-
-# Streamlit interface
-st.title("Annotation Tool")
-
-# Feature toggles
-st.sidebar.title("Additional Features")
-enable_file_uploader = st.sidebar.checkbox("Enable file uploader", value=True)
-enable_progress_indicator = st.sidebar.checkbox("Enable progress indicator", value=True)
-enable_visualizations = st.sidebar.checkbox("Enable visualizations", value=True)
-
-if enable_file_uploader:
-    st.write("Please upload your dataset and configuration files.")
-    dataset_file = st.file_uploader("Upload dataset.jsonl", type=["jsonl"])
-    schema_file = st.file_uploader("Upload schema.txt", type=["txt"])
-
-    if dataset_file and schema_file:
-        dataset_path = Path("uploaded_dataset.jsonl")
-        schema_path = Path("uploaded_schema.txt")
-
-        with open(dataset_path, "wb") as f:
-            f.write(dataset_file.getbuffer())
-
-        with open(schema_path, "wb") as f:
-            f.write(schema_file.getbuffer())
-
-        main_app()
-else:
-    main_app()
-
-import os
+    # Original code adapted for Streamlit
+    # ... (All the original code, but with input and output calls replaced with Streamlit components) ...
+    import os
 import time
 import json
 import uuid
@@ -215,6 +183,33 @@ os.system('clear')
 
 st.write("Annotation terminée, merci de renvoyer l'ensemble du dossier pour évaluation.")
         
+# Streamlit interface
+st.title("Annotation Tool")
+
+# Feature toggles
+st.sidebar.title("Additional Features")
+enable_file_uploader = st.sidebar.checkbox("Enable file uploader", value=True)
+enable_progress_indicator = st.sidebar.checkbox("Enable progress indicator", value=True)
+enable_visualizations = st.sidebar.checkbox("Enable visualizations", value=True)
+
+if enable_file_uploader:
+    st.write("Please upload your dataset and configuration files.")
+    dataset_file = st.file_uploader("Upload dataset.jsonl", type=["jsonl"])
+    schema_file = st.file_uploader("Upload schema.txt", type=["txt"])
+
+    if dataset_file and schema_file:
+        dataset_path = Path("uploaded_dataset.jsonl")
+        schema_path = Path("uploaded_schema.txt")
+
+        with open(dataset_path, "wb") as f:
+            f.write(dataset_file.getbuffer())
+
+        with open(schema_path, "wb") as f:
+            f.write(schema_file.getbuffer())
+
+        main_app()
+else:
+    main_app()
 
 # Add progress indicators and visualizations as needed
 if enable_progress_indicator or enable_visualizations:
